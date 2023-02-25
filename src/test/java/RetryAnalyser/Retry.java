89263abfbadd4b974b69487b91692139d3ac71d9
Bharani <bharaniravi74@gmail.com>
2023-02-25 13:39:42 +0530

    Week 2 Day 1

---
+++ src/test/java/RetryAnalyser/Retry.java
@@ -0,0 +1,26 @@
+package RetryAnalyser;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer{
+
+	  private int count = 0; 
+
+	  private int maxCount = 2;
+
+	  public boolean retry(ITestResult result) { 
+
+	    if(count < maxCount) {  
+
+	       count++;
+
+	       return true;        
+
+	    }        
+
+	    return false; 
+	
+
+}
+}
